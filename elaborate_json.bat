cf8353d2b97d26b8bc3a4230c512750e3a9e6844
Samuel Ellicott <ellicott.4@osu.edu>
2024-10-30 15:58:19 -0400

    Add comments to utility scripts

--- elaborate_json.bat
+++ elaborate_json.bat
@@ -1,3 +1,10 @@
+:: Run Yosys to generate an elaborated netlist of a Verilog Module
+:: JSON output is copied to clipboard for pasting into netlistsvg
+:: Author: Sam Ellicott
+:: Date: October 30, 2024
+:: Useage: elaborate_json.bat <top level module> <input file>
+
+:: Check parameters and generate useage message
 @echo off
 IF "%~1"=="" GOTO useage 
 IF "%~2"=="" GOTO useage
@@ -7,7 +14,10 @@ GOTO run
 GOTO end
 
 :run
+:: Run yosys and generate the json output file
 yosys -p "prep -top %1; write_json output.json" %2 
+
+:: copy JSON output to clipboard
 type output.json | clip
 @echo "JSON copied to clipboard"
 :end
