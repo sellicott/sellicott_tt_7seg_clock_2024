5354b80f879990cd42373c23965c741660ceca25
Samuel Ellicott <ellicott.4@osu.edu>
2024-11-04 20:35:43 -0500

    update elaboration script

--- elaborate_json.bat
+++ elaborate_json.bat
@@ -13,9 +13,25 @@ GOTO run
 @echo "Useage: <top level module> <input file>" 
 GOTO end
 
+:: 
 :run
+set top_module=%~1
+
+:: put all other inputs into a single variable
+shift
+set file_list="%~1"
+:loop
+shift
+if "%~1"=="" goto afterloop
+set file_list=%file_list% "%~1"
+goto loop
+:afterloop
+
+@echo %top_module%
+@echo %file_list%
+
 :: Run yosys and generate the json output file
-yosys -p "prep -top %1; write_json output.json" %2 
+yosys -p "prep -top "%top_module%"; write_json output.json" %file_list% 
 
 :: copy JSON output to clipboard
 type output.json | clip
